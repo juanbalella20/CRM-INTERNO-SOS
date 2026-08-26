7a393389f315603e6c532053f2eead47140952f5
juanbalella20 <jbalella@fi.uba.ar>
2026-08-26 10:31:48 -0300

    add: definicion de html sin estilo de

---
+++ code/frontend/src/lib/components/Sidebar.svelte
@@ -0,0 +1,17 @@
+<!-- ruta de navegacion: identifica una pagina denro de la aplicacion -->
+<!-- Cuando el usuario toca el enlace:
+1. El navegador navega a /configuracion.
+2. SvelteKit busca la página correspondiente.
+3. La muestra en pantalla.
+En SvelteKit, la relación sería:
+URL:     /configuracion
+Archivo: src/routes/configuracion/+page.svelte-->
+
+
+<aside class = "sidebar">
+
+    <nav aria-label="Navegacion principal">
+      <a href = "/">Gestion de prospectos</a>
+      <a href = "/configuracion">Configuracion</a>  
+    </nav>
+</aside>
